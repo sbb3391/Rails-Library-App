9651c1445cbb5df79ec037278f3e0d3c82df8181
sbb3391 <sbb33391@gmail.com>
2021-05-04 14:53:48 -0400

    added book helper checkout_class

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def checkout_class(book)
+    if book.checked_out?
+      "This is the class info for a book that's already reserved"
+    else
+      "This is the class for a book that is available for checkout"
+    end
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,12 @@ class Book < ApplicationRecord
   has_many :book_transactions
   has_many :media_transactions, through: :book_transactions
 
+  validates :title, presence: :true
+  validates :author, presence: :true
+  validates :description, presence: :true, length: {minimum: 100 }, allow_blank: false
+  
+  # isbn regex
+  # /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/
 
   def checked_out?
     true if self.library_id != nil
